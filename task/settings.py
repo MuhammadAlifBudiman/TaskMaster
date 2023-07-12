9397485ee876f387d290bacd3e096ea9cabb0330
Muhammad Alif Budiman <alif@LAPTOP-KN2RMG13.localdomain>
2023-07-12 09:15:55 +0700

    Fix Configuration

--- task/settings.py
+++ task/settings.py
@@ -21,12 +21,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-0lj9$j6#k#o^3l(jr4vr1as@wg41b9rcadx&qmdc$6xx91z0eb'
+SECRET_KEY = config('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = [*]
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
